d975058343c99686c9a6b1560af36c1c25fa2689
ykmxxi <ykmxxi97@gmail.com>
2024-09-03 16:03:32 +0900

    요청 캐시: RequestCache, SavedRequest

--- spring-security-master/src/main/java/io/security/springsecuritymaster/IndexController.java
+++ spring-security-master/src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -5,14 +5,18 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
     @GetMapping("/")
-    public String index() {
-        return "Login Success: index";
+    public String index(@RequestParam(name = "customParam") String customParam) {
+        if (customParam == null) {
+            return "Login Success: index";
+        }
+        return "Login Success: customParam exists";
     }
 
     @GetMapping("/home")
--- spring-security-master/src/main/java/io/security/springsecuritymaster/config/SecurityConfig.java
+++ spring-security-master/src/main/java/io/security/springsecuritymaster/config/SecurityConfig.java
@@ -2,19 +2,16 @@ package io.security.springsecuritymaster.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.context.SecurityContextHolderStrategy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -24,34 +21,18 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+        HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
+        requestCache.setMatchingRequestParameterName("customParam=y");
+
         http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers("logoutSuccess").permitAll()
                         .anyRequest().authenticated())
-//                .csrf(csrf -> csrf.disable()) // GET 방식 로그아웃을 사용하려면 csrf 기능 off
-                .formLogin(Customizer.withDefaults())
-                .logout(logout -> logout
-                        .logoutUrl("/logout") // 기본 POST 방식
-                        // RequestMatcher가 logoutUrl()보다 높은 우선권
-                        // GET 방식 로그아웃을 허용하려면 "GET" 지정 or HTTP 메서드를 지정하지 않으면 된다
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "POST"))
-                        .logoutSuccessUrl("/logoutSuccess")
-                        // LogoutSuccessHandler가 logoutSuccessUrl()보다 높은 우선권
-                        .logoutSuccessHandler((request, response, authentication) -> {
-                            response.sendRedirect("logoutSuccess");
-                        })
-                        .deleteCookies("JSESSIONID", "remember-me")
-                        .invalidateHttpSession(true) // HttpSession 무효화
-                        .clearAuthentication(true) // SecurityContextLogoutHandler가 인증을 삭제
-                        .addLogoutHandler((request, response, authentication) -> {
-                            HttpSession session = request.getSession();
-                            session.invalidate();
-                            SecurityContextHolderStrategy securityContextHolder = SecurityContextHolder.getContextHolderStrategy();
-                            securityContextHolder.getContext()
-                                    .setAuthentication(null);
-                            securityContextHolder.clearContext();
-                        })
-                        .permitAll()
-                );
+                .formLogin(form -> form
+                        .successHandler((request, response, authentication) -> {
+                            SavedRequest savedRequest = requestCache.getRequest(request, response);
+                            String redirectUrl = savedRequest.getRedirectUrl();
+                            response.sendRedirect(redirectUrl);
+                        }))
+                .requestCache(cache -> cache.requestCache(requestCache));
 
         return http.build();
     }
