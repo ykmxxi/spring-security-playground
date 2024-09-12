6490bcc1fba35bf81bdcac42f8b5541942152eb7
ykmxxi <ykmxxi97@gmail.com>
2024-09-12 17:49:19 +0900

    Spring MVC 로그인 구현

--- spring-security-master/src/main/java/io/security/springsecuritymaster/mvc/LoginController.java
+++ spring-security-master/src/main/java/io/security/springsecuritymaster/mvc/LoginController.java
@@ -27,14 +27,19 @@ public class LoginController {
             final HttpServletRequest request,
             final HttpServletResponse response
     ) {
+        // 사용자 이름과 비밀번호를 담은 인증 객체를 생성
         UsernamePasswordAuthenticationToken token = UsernamePasswordAuthenticationToken.unauthenticated(
                 loginRequest.username(), loginRequest.password());
+        // 인증을 시도하고 최종 인증 결과를 반환
         Authentication authentication = authenticationManager.authenticate(token);
 
+        // 인증 정보 유지를 위해 SecurityContext 생성 및 저장
         SecurityContext securityContext = SecurityContextHolder.getContextHolderStrategy().createEmptyContext();
-        securityContext.setAuthentication(authentication);
+        securityContext.setAuthentication(authentication); // 인증 결과를 컨텍스트에 저장
+        // 컨텍스트를 ThreadLocal 에 저장
         SecurityContextHolder.getContextHolderStrategy().setContext(securityContext);
 
+        // 컨텍스트를 세션에 저장해서 인증 상태를 영속
         securityContextRepository.saveContext(securityContext, request, response);
         return authentication;
     }
